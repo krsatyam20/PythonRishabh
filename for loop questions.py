c1b9f47a6b4ed3b28a5fc6082ce63dc7dbc82289
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-05-27 17:05:49 +0530

    Add files via upload

---
+++ for loop questions.py
@@ -0,0 +1,18 @@
+
+#ans 2
+
+'''a=0
+for i in range(1,11):
+    a=a+i
+print(a)
+'''
+
+# 1*2=2
+
+a=2
+for i in range(1,11):
+   print("%d * %d= %d" %(i,a,a*i))
+
+   
+   
+   
---
+++ forloop.py
@@ -0,0 +1,61 @@
+''' loop : loop is a reserved keyword it`s used for repetaion of any task
+
+
+types of loop
+
+1. For loop
+2.while loop
+
+
+
+'''
+
+for i in range(0,10):
+    print("Rishab %d " %(i))
+
+
+
+for i in range(1,21):
+    print(i)
+
+
+#use break keyword
+print("===========break keyword=================")
+for i in range(1,21):
+    if(i==8):
+        break;
+        
+    print(i)
+
+
+#continue
+
+print("===========continue keyword=================")
+
+
+for d in range(1,21):
+    if(d==12):
+        continue;
+        
+    print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
